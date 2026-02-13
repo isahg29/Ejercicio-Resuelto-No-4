be37b8eadae61b63e12af6bbda6b5ae8f44826c0
isahg29 <ihoyos@unal.edu.co>
2026-02-13 00:27:02 -0500

    Add files via upload

---
+++ EjercicioResuelto4.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicioresuelto4;
+
+
+public class EjercicioResuelto4 {
+
+    public static void main(String[] args) {
+        double edalber, edana, edmama;
+        double edjuan = 9;
+        edalber = edades.calcular_edalber(edjuan);
+        edana = edades.calcular_edana(edjuan);
+        edmama = edades.calcular_edmama(edjuan, edalber, edana);
+        System.out.println("las edades son; Alberto= "+ edalber + " Juan= " + edjuan + " Ana= " + edana + " Mama= " + edmama);
+    }
+}
